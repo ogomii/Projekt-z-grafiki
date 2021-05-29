fd47a0a15b5de668883ffe669217c570800bc2da
karokotlowska <62810585+karokotlowska@users.noreply.github.com>
2021-05-29 15:18:46 +0200

    Update GUI.cpp

--- GUI.cpp
+++ GUI.cpp
@@ -90,8 +90,10 @@ MyFrame1::MyFrame1(wxWindow* parent, wxWindowID id, const wxString& title, const
 	wxBoxSizer* bSizer8;
 	bSizer8 = new wxBoxSizer(wxVERTICAL);
 
-	m_textCtrl1 = new wxTextCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize(372, -1), wxTE_MULTILINE | wxTE_READONLY | wxTE_RIGHT | wxTE_WORDWRAP);
+	m_textCtrl1 = new wxStaticText(this, wxID_ANY, wxT(""), wxDefaultPosition, wxDefaultSize, 0);
+	m_textCtrl1->Wrap(-1);
 	bSizer8->Add(m_textCtrl1, 0, wxALL, 5);
+	
 	bSizer7->Add(bSizer8, 1, wxEXPAND, 5);
 
 	wxBoxSizer* bSizer10;
